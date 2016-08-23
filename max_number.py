3c00d6f729c6537a66be9ecf6c1b633bdd733683
Mitchell Rapp <project.space.000@gmail.com>
2016-08-23 17:36:35 +0000

    added max_number.py

---
+++ max_number.py
@@ -0,0 +1,9 @@
+number1 = input("Give me a number: ")
+number2 = input("Give me another number: ")
+
+if number1 > number2:
+    print "Your first number is larger than your second number"
+elif number1 < number2:
+    print "Your second number is larger than your first number"
+else:
+    print "Your numbers are the same"
