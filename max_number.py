70b93a14e5cb10b51e05297728793468d6c49387
Mitchell Rapp <project.space.000@gmail.com>
2016-08-25 16:27:49 +0000

    started validating user inputs for max_number

--- max_number.py
+++ max_number.py
@@ -1,5 +1,14 @@
-number1 = input("Give me a number: ")
-number2 = input("Give me another number: ")
+number1 = False
+number2 = False
+
+while type(number1) != int:
+    number1 = input("Give me a number: ")
+
+while type(number2) != int:
+    number2 = input("Give me another number: ")
+
+# number1 = input("Give me a number: ")
+# number2 = input("Give me another number: ")
 
 if number1 > number2:
     print "Your first number is larger than your second number"
