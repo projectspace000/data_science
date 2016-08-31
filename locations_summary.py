fd207a912ca353efd56d7377c33a3a7f0386623c
Mitchell Rapp <project.space.000@gmail.com>
2016-08-30 23:02:42 -0600

    updated locations details summary script

--- locations_summary.py
+++ locations_summary.py
@@ -9,13 +9,24 @@ locations = pd.read_csv("C:\\Users\\flynn\\Desktop\\DataScience\\locations.csv",
 # remove unwanted columns
 print("Starting Columns\n")
 list(locations.columns.values)
-locations.drop(["locationCode1", "County", "siteId", "terminalId", "SiteStartDate", "latitude", "longitude"], inplace = True, axis = 1)
+
+locations.drop(["locationCode1", "County", 
+				"siteId", "terminalId", 
+				"SiteStartDate", "latitude", 
+				"longitude"], inplace = True, axis = 1)
+
 print("Remaining Columns\n")
 list(locations.columns.values)
 
 # rename columns using proper name and case
 # i need to find the right way to wrap this, toooooo loooong
-locations.rename(columns={"Territory3" : "Territory", "FSR2" : "FSR", "storeNumber1" : "Store", "LocationName" : "Location", "addressLine1" : "Address", "city" : "City", "state" : "State", "phoneNumber" : "Phone", "SerialNumber" : "Kiosk", "placement" : "Placement"}, inplace=True)
+locations.rename(columns={"Territory3" : "Territory", 
+						"FSR2" : "FSR", "storeNumber1" : "Store",
+						"LocationName" : "Location", "addressLine1" : "Address",
+						"city" : "City", "state" : "State",
+						"phoneNumber" : "Phone", "SerialNumber" : "Kiosk",
+						"placement" : "Placement"}, inplace=True)
+
 print("Cleaned Columns\n")
 list(locations.columns.values)
 
